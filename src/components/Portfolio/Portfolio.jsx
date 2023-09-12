bd923015d7fc983187762fd155d85f57267a738b
dculbert89 <dculbert89@gmail.com>
2023-09-12 14:42:02 -0400

    projects open in new tab

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -3,6 +3,7 @@ import "./portfolio.css";
 import Portfolio1 from "../../assets/portfolio-1.svg";
 import Portfolio2 from "../../assets/portfolio-2.svg";
 import Portfolio3 from "../../assets/portfolio-3.svg";
+import Portfolio4 from "../../assets/portfolio-4.svg";
 
 export const Portfolio = () => {
   return (
@@ -16,20 +17,39 @@ export const Portfolio = () => {
       </span>
 
       <div className="worksImgs">
-        <a href="https://dculbert89-weather.netlify.app/">
+        <a
+          href="https://dculbert89-weather.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src={Portfolio1} alt="Portfolio1" className="worksImg" />
         </a>
 
-        <a href="https://culbertson-todo.netlify.app/">
+        <a
+          href="https://culbertson-todo.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src={Portfolio2} alt="Portfolio2" className="worksImg" />
         </a>
 
-        <a href="https://pageturnersreviews.netlify.app/">
+        <a
+          href="https://pageturnersreviews.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src={Portfolio3} alt="Portfolio3" className="worksImg" />
         </a>
 
-
+        <a
+          href="https://culbertson-clock.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={Portfolio4} alt="Portfolio3" className="worksImg" />
+        </a>
       </div>
+
       {/* <button className="worksBtn">See More</button> */}
     </section>
   );
